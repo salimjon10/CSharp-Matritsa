8f1a1cc12131e149432a5bce8c77a3cdbdfea68e
salimjon10 <salimjonrahimov10@mail.ru>
2023-03-19 13:30:48 +0700

    Оптимизировал код

--- Program.cs/Class1.cs
+++ Program.cs/Class1.cs
@@ -1,8 +1,77 @@
-﻿using System;
+﻿/*********************************
+ * Лабораторная работа №3        *
+ * Автор:  Рахимов С.О           *
+ *      Дата :16.03.23           *
+ ********************************/
 
-namespace Program.cs
+using System;
+
+namespace MatrixCalculator
 {
-    public class Class1
+    class Program
     {
+        static void Main(string[] args)
+        {
+            var random = new Random();
+
+            // Создаем матрицы для тестирования
+            var MatrixA = new Matrix(3);
+            for (int ColumnCounter = 0; ColumnCounter < 3; ++ColumnCounter)
+            {
+                for (int RowCounter = 0; RowCounter < 3; ++RowCounter)
+                {
+                    MatrixA[ColumnCounter, RowCounter] = random.Next(10);
+                }
+            }
+            Console.WriteLine($"MatrixA =\n{MatrixA}");
+
+            var MatrixB = new Matrix(3);
+            for (int ColumnCounter = 0; ColumnCounter < 3; ++ColumnCounter)
+            {
+                for (int RowCounter = 0; RowCounter < 3; ++RowCounter)
+                {
+                    MatrixB[ColumnCounter, RowCounter] = random.Next(10);
+                }
+            }
+            Console.WriteLine($"MatrixB =\n{MatrixB}");
+
+            // Примеры использования операторов и методов
+            Console.WriteLine($"MatrixA + MatrixB =\n{MatrixA + MatrixB}");
+
+            Console.WriteLine($"MatrixA * MatrixB =\n{MatrixA * MatrixB}");
+
+            Console.WriteLine($"MatrixA > MatrixB: {MatrixA > MatrixB}");
+            Console.WriteLine($"MatrixA >= MatrixB: {MatrixA >= MatrixB}");
+            Console.WriteLine($"MatrixA <= MatrixB: {MatrixA <= MatrixB}");
+            Console.WriteLine($"MatrixA == MatrixB: {MatrixA == MatrixB}");
+            Console.WriteLine($"MatrixA != MatrixB: {MatrixA != MatrixB}");
+
+            Console.WriteLine($"Determinant of MatrixA: {MatrixA.Determinant()}");
+
+            try
+            {
+                var inverseA = MatrixA.Inverse();
+                Console.WriteLine($"Inverse of MatrixA:\n{inverseA}");
+            }
+            catch (MatrixNotInvertibleException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            // Проверка работы прототипа глубокого копирования
+            var c = MatrixA.Clone();
+            Console.WriteLine($"C =\n{c}");
+            Console.WriteLine($"MatrixA == C: {MatrixA == c}");
+
+            // Проверка обработки пользовательских исключений
+            try
+            {
+                var d = new Matrix(0);
+            }
+            catch (InvalidMatrixSizeException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
     }
 }
